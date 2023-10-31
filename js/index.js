4227742c1521c2b782238f05f679d50aced1446e
Briannlool <brian.chan080@gmail.com>
2023-10-30 22:38:23 -0400

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,9 @@
+// make a preloader 
+ScrollReveal().reveal('.largeCards', {delay: 50});
+ScrollReveal().reveal('.projSect', {delay: 150});
+
+
+// $('.navbar-toggler').click(function(){
+//     $('.navSpace').css('background-color', '#64a0c0');
+
+// });
